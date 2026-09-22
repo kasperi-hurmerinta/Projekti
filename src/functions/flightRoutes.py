9218efbd46a46b3f148d212c92c60266cd83857b
Kaap0p <kaap0p@outlook.com>
2026-09-22 12:13:43 +0300

    Sanakirja tehty!

---
+++ src/functions/flightRoutes.py
@@ -0,0 +1,6 @@
+flightroutes= {
+    1: ["Suomi", "Ruotsi", "Saksa", "Italia", "Kreikka"],
+    2: ["Suomi", "Liettua", "Puola", "Romania", "Bulgaria"],
+    3: ["Suomi", "Ruotsi", "Norja", "Irlanti", "Yhdysvallat"],
+    4: ["Suomi", "Turkki", "Kiina", "Etelä-Korea", "Japani"],
+}

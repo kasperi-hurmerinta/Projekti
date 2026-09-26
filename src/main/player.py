5a1f01491b7713a44d28dd804830a1eb0a2f79f5
WarumDach <warumdach@gmail.com>
2026-09-26 11:04:06 +0300

    Olioon lisätty lisää tietoja. Eikö tähän olioon kannata myös tallentaa pisteet sekä sijainti niin ei tarvitse tehdä funktiota esim. (tarkista_pisteet) ?

--- src/main/player.py
+++ src/main/player.py
@@ -1,6 +1,10 @@
 current_player = None
 
 class Player:
-    def __init__(self, screen_name, id):
+    def __init__(self, screen_name, id, score, location):
         self.screen_name = screen_name
         self.id = id
+        self.score = score
+        self.location = location
+
+## Eikö tähän olioon kannata myös tallentaa pisteet sekä sijainti niin ei tarvitse tehdä funktiota esim. (tarkista_pisteet) ? - Kasperi
